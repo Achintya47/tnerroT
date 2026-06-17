bbba49d1317a77c12fc340d239347bda595a79f5
Achintya Sharma <sharmaachintya49@gmail.com>
2026-06-18 02:40:40 +0530

    Update : main.c

--- src/main.c
+++ src/main.c
@@ -5,21 +5,58 @@
 #include "torrent.h"
 #include "bencoder.h"
 
+char* read_file(const char* path, size_t* size_out) {
+    FILE* fp = fopen(path, "rb");
+
+    if (!fp)
+        return NULL;
+
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    rewind(fp);
+
+    char* buffer = malloc(size);
+
+    if (!buffer) {
+        fclose(fp);
+        return NULL;
+    }
+
+    if (fread(buffer, 1, size, fp) != (size_t)size) {
+        free(buffer);
+        fclose(fp);
+        return NULL;
+    }
+
+    fclose(fp);
+
+    *size_out = (size_t)size;
+
+    return buffer;
+}
+
 int main(int argc, char* argv[]) {
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <torrent-file>\n", argv[0]); 
         return EXIT_FAILURE;
     }
-    
-    FILE* fp = fopen(argv[1], "rb");
-    if (!fp) {
-        perror("fopen");
-        return EXIT_FAILURE;
-    }
-    
-    BValue* root = decode_value(fp);
+    size_t file_size;
 
-    fclose(fp);
+    char* buffer =
+        read_file(argv[1], &file_size);
+
+    if (!buffer)
+        return EXIT_FAILURE;
+
+    Parser parser = {
+        .data = buffer,
+        .size = file_size,
+        .pos = 0
+    };
+
+    BValue* root =
+        decode_value(&parser);
 
     if (!root) {
         destroy_value(root);
@@ -30,15 +67,23 @@ int main(int argc, char* argv[]) {
     Torrent* torrent = torrent_parse(root);
 
     if (!torrent) {
-        destroy_value(root);
-        torrent_destroy(torrent);
         fprintf(stderr, "Failed to parse torrent metadata\n");
+        destroy_value(root);
+        free(buffer);
+        return EXIT_FAILURE;
     }
 
-    torrent_print(torrent);
+
+    /* since w're now testing info_hash cretion, this shall be removed */
+    // torrent_print(torrent);
+
+    // connect_tracker(torrent);
     
     destroy_value(root);
+
     torrent_destroy(torrent);
+
+    free(buffer);
 
     return 0;
 }
